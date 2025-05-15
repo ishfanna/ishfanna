a3bd483d9685a321af98a2ab644df8c54511152e
ishfanna <beyawoozdie@gmail.com>
2025-05-15 21:07:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,4 @@ st.title("hello this is udinara")
 st.image("20250515_193947.jpg",width=200)
 st.write("/n")
 st.subheader("ishfanna")
-st.write(
-   "dinara love asni") 
+st.write("dinara love asni") 
