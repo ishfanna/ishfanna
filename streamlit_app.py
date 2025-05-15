977c38bfafa3bd3493bdc105ad3f7117d437c544
ishfanna <beyawoozdie@gmail.com>
2025-05-15 20:24:15 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("hello this is udinara")
 st.write(
-    "ini foto aku sama asni"
+    "ini  foto aku sama asni"
 )
 st.image("20250515_193947.jpg", width=200)
 st.write("\n")
