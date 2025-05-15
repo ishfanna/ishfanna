f2563c5ae230b0c5026f45f1722e2af3c00247b1
ishfanna <beyawoozdie@gmail.com>
2025-05-15 20:11:14 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("hello this is udinara")
+st.write(
+    "Below is a photo of me and Asni"
+)
+st.image("views/20250515_193947.jpg", width=200)
+st.write("\n")
+st.subheader("dindondun")
+st.write(
+    "dinara love asni"
+) 
+ 
